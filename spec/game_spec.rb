5ba799b74d9d39cd39231aeb150d84de241917c9
thejwuscript <88938117+thejwuscript@users.noreply.github.com>
2022-01-08 13:45:45 +0000

    Write a test for #choose_color

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,13 +6,31 @@ require_relative '../lib/grid'
 # rubocop:disable Metrics/BlockLength
 
 describe Game do
+  subject(:game) { described_class.new }
+  
   describe '#initialize' do
-    subject(:create_game) { described_class.new }
-    
     it 'instantiates class Grid' do
       expect(Grid).to receive(:new)
-      create_game
+      game
     end
+  end
+
+  describe '#choose_color' do
+    context 'when argument is 1' do
+      it 'assigns red to player' do
+        game.choose_color
+        color = game.player.color
+        expect(color).to eq('🔴')
+      end
+
+      it 'assigns yellow to CPU' do
+        game.choose_color
+        color = game.computer.color
+        expect(color).to eq('🟡')
+      end
+    end
+  
+  
   
   # choose who goes first.
   # heads or tails.
