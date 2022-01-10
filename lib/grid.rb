945dedb83f7155619b7beadb1019c8b13d9144d2
thejwuscript <88938117+thejwuscript@users.noreply.github.com>
2022-01-10 12:48:58 +0000

    Create and test #place_color_in_slot

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,10 +1,11 @@
 #frozen_string_literal: true
 
 class Grid
-
+  attr_accessor :slots_layout
   LETTERS = %w[A B C D E F G]
   
   def initialize
+    @slots_layout = Array.new(6) { Array.new(7, ' ') }
   end
 
   def update_slots(color, coordinate)
@@ -14,9 +15,15 @@ class Grid
   end
 
   def input_to_grid_position(coordinate)
-    y_coordinate = LETTERS.index( coordinate[0] )
-    x_coordinate = -coordinate[1].to_i
+    x_coordinate = LETTERS.index( coordinate[0] )
+    y_coordinate = -coordinate[1].to_i
     grid_position = [x_coordinate, y_coordinate]
+  end
+
+  def place_color_in_slot(color, coordinate)
+    x = coordinate[0]
+    y = coordinate[1]
+    self.slots_layout[y][x] = color
   end
 
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -9,19 +9,39 @@ describe Grid do
   #  it 'updates grid with given coordinate and color' do
   #    color = '🔴'
   #    coordinate = 'B3'
+  #    grid_position = [-3, 1]
+  #    allow(grid).to receive(:input_to_grid_position).with(coordinate).and_return([grid_position)
   #    grid.update_slots(color, coordinate)
-  #    updated = @grid_nested_array[-3][1]
+  #    updated = @slots_layout[-3][1]
   #    expect(updated).to eq('🔴')
   #  end  
   #end
 
   describe '#input_to_grid_position' do
-    it 'converts player input as an argument into a two-element array' do
-      input = 'B3'
-      array = grid.input_to_grid_position(input)
-      expect(array).to eq( [-3, 1] )
+    it 'converts B3 as an argument into a two-element array' do
+      player_input = 'B3'
+      array = grid.input_to_grid_position(player_input)
+      expect(array).to eq( [1, -3] )
+    end
+
+    it 'converts G5 as an argument into a two-element array' do
+      player_input = 'G5'
+      array = grid.input_to_grid_position(player_input)
+      expect(array).to eq( [6, -5] )
     end
   end
 
+  describe '#place_color_in_slot' do
+    it 'Takes two arguments, use them to find the correct nested array index and place color there' do
+      color = '🔴'
+      grid_position = [1, -3]
+      grid.place_color_in_slot(color, grid_position)
+      slot = grid.slots_layout[-3][1]
+      expect(slot).to eq(color)
+    end
+  
+  end
+
+    
 
 end
